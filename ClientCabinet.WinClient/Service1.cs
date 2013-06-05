3c2820778367ced96a00b951be742efabae1d01d
Katsiaryna Tsalabanava <katsiaryna_tsalabanava@epam.com>
2013-06-05 12:31:01 +0300

    feature 1 was implemented

--- ClientCabinet.WinClient/Service1.cs
+++ ClientCabinet.WinClient/Service1.cs
@@ -10,9 +10,9 @@ using System.Threading.Tasks;
 
 namespace ClientCabinet.WinClient
 {
-	public partial class Service1 : ServiceBase
+	public partial class Service : ServiceBase
 	{
-		public Service1()
+		public Service()
 		{
 			InitializeComponent();
 		}
