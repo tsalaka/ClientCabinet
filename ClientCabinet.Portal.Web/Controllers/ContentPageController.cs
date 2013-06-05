522ce0e2d940c3d4dbf9b0ab740956e8f68539be
Katsiaryna Tsalabanava <katsiaryna_tsalabanava@epam.com>
2013-06-05 12:55:01 +0300

    controllers were added

---
+++ ClientCabinet.Portal.Web/Controllers/ContentPageController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RC.Content.Core.BLL;
+using RGSiteCore.SM.NodeTypes;
+
+namespace RC.SiteCore.Engine.Controllers
+{
+    public class ContentPageController : Controller
+    {
+        //
+        // GET: /ContentPage/
+
+        public ActionResult Index()
+        {
+	        var currentNode = RCSiteCore.RC.SiteMapFactory.CurrentNode;
+			if(currentNode is IPageNode)
+			{
+				var html = HtmlContentManager.GetHtmlContent(
+					Server.MapPath(String.Format("~/SharedFiles/Schema/{0}.xml", ((IPageNode) currentNode).PageGuid)));
+				ViewBag.Source = html.Source;
+			}
+	        
+            return View();
+        }
+
+		public void Redirect()
+		{
+			var currentNode = RCSiteCore.RC.SiteMapFactory.CurrentNode;
+			if (currentNode is IRedirectNode)
+			{
+				if (currentNode != null && !String.IsNullOrEmpty(currentNode.Href))
+				{
+					Response.Redirect(currentNode.Href);
+				}
+				
+			}
+
+			Redirect("/");
+		}
+
+    }
+}
---
+++ ClientCabinet.Portal.Web/Controllers/PagerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RC.SiteCore.Engine.Controllers
+{
+    public class PagerController : Controller
+    {
+        //
+        // GET: /Pager/
+
+        public ActionResult Index(int currentPage,int itemsCount, int rowsCount, string action, string controller)
+        {
+	        ViewBag.PagerCount = Convert.ToInt32(itemsCount/rowsCount);
+	        ViewBag.Action = action;
+	        ViewBag.Controller = controller;
+            return PartialView();
+        }
+
+		public ActionResult AjaxIndex(int currentPage, int itemsCount, int rowsCount, string action, string controller, string updateTargetId)
+		{
+			ViewBag.PagerCount = Convert.ToInt32(itemsCount / rowsCount);
+			ViewBag.Action = action;
+			ViewBag.Controller = controller;
+			ViewBag.UpdateTargetId = updateTargetId;
+			ViewBag.CurrentPage = currentPage;
+			RedirectToAction(action, controller);
+			return PartialView();
+		}
+
+		/*[HttpPost]
+		public ActionResult Index(int currentPage)
+		{
+			ViewBag.CurrentPage = currentPage;
+			RedirectToAction(action, controller);
+			return PartialView();
+		}*/
+    }
+}
