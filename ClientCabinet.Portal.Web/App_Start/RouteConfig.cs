c8ffeb70a18c5a066080b60bd17c55fddfe22ed0
Katsiaryna Tsalabanava <katsiaryna_tsalabanava@epam.com>
2013-06-30 09:49:14 +0300

    Add Home view

--- ClientCabinet.Portal.Web/App_Start/RouteConfig.cs
+++ ClientCabinet.Portal.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ClientCabinet.Portal.Web
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "Home", action = "Index2", id = UrlParameter.Optional }
 			);
 		}
 	}
